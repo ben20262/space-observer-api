4ee9861b0cafddcd89503c8bf0c4222c36ee83e2
ben20262 <ben20262@aim.com>
2020-07-20 13:30:49 -0400

    added url to visible objects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_204018) do
+ActiveRecord::Schema.define(version: 2020_07_14_225456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_204018) do
     t.string "magnitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
 end

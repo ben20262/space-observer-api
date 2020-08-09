6acc9a32253b13dd2ed714895b55a03caea8c19f
ben20262 <ben20262@aim.com>
2020-08-09 15:44:24 -0400

    added bino index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'visible_objects#index'
+
+  resources :binocular_objects, only: [:index]
+  
 end

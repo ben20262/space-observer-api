c6cb9324f8212214d66380f6de7d99afae6e953d
ben20262 <ben20262@aim.com>
2020-09-20 13:27:33 -0400

    added show routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'visible_objects#index'
 
-  resources :binocular_objects, only: [:index]
-  resources :small_telescope_objects, only: [:index]
+  resources :binocular_objects, only: [:index, :show]
+  resources :small_telescope_objects, only: [:index, :show]
 end
